d4de6b824ed4e251700a6af932d4bf89344d888e
Vage <lukjanovvalera@gmail.com>
2020-04-03 23:09:46 +0300

    first

---
+++ src/main/java/koschei/models/Egg6.java
@@ -0,0 +1,20 @@
+package koschei.models;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Egg6 {
+
+    Needle7 needle7;
+
+    @Override
+    public String toString() {
+        return ", в яйце иголка " + needle7.toString();
+    }
+
+    @Autowired
+    public void setNeedle7(Needle7 needle7) {
+        this.needle7 = needle7;
+    }
+}
